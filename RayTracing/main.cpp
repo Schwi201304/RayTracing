40fe39acdd3e1dd1c285f7e7a32b4981dbbb447e
Schwi <oyq201304@gmail.com>
2022-03-23 10:08:16 +0800

    Motion Blur

--- RayTracing/main.cpp
+++ RayTracing/main.cpp
@@ -4,6 +4,7 @@
 #include "sphere.h"
 #include "camera.h"
 #include "material.h"
+#include "moving_sphere.h"
 #include <iostream>
 
 color ray_color(const ray& r, const hittable& world, int depth) {
@@ -42,7 +43,9 @@ hittable_list random_scene() {
 					// diffuse
 					auto albedo = random() * random();
 					sphere_material = make_shared<lambertian>(albedo);
-					world.add(make_shared<sphere>(center, 0.2, sphere_material));
+					auto center2 = center + vec3(0, random_double(0, .5), 0);
+					world.add(make_shared<moving_sphere>(
+						center, center2, 0.0, 1.0, 0.2, sphere_material));
 				}
 				else if (choose_mat < 0.95) {
 					// metal
@@ -76,8 +79,8 @@ int main() {
 
 	// Image
 
-	const auto aspect_ratio = 3.0 / 2.0;
-	const int image_width = 600;
+	const auto aspect_ratio = 16.0/9.0;
+	const int image_width = 400;
 	const int image_height = static_cast<int>(image_width / aspect_ratio);
 	const int samples_per_pixel = 100; 
 	const int max_depth = 50;
@@ -94,7 +97,7 @@ int main() {
 	auto dist_to_focus = 10.0;
 	auto aperture = 0.1;
 
-	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+	camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus,0,1.0);
 
 	// Render
 	Image img(image_width, image_height);
